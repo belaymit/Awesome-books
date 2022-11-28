a3b28deb254f8ca47a67b763cb13ee226fb1610a
belaymit <2belamit@gmail.com>
2022-11-28 19:49:18 +0300

    added displyabook function

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ const form = document.querySelector('#book-form');
 const bookTitle = document.querySelector('#bookTitle');
 const authorName = document.querySelector('#authorName');
 const isbnNumber = document.querySelector('#isbnNumber');
+const allBooks = document.querySelector('.all-books');
+
+function displayBook() {
+  const allBook = bookCollection.map((item) => `<div class="book">
+    <h3>${item.bookTitle}</h3>
+    <p>
+     ${item.authorName}
+    </p>
+    <button class="btn">Remove</button>
+  </div>`);
+  allBooks.innerHTML = allBook;
+}
+
 const addBook = (e) => {
   e.preventDefault();
   const addNewBookData = {
@@ -13,6 +26,7 @@ const addBook = (e) => {
     isbnNumber: isbnNumber.value,
   };
   bookCollection.push(addNewBookData);
+  displayBook();
 };
 
 submitBtn.addEventListener('click', addBook);
