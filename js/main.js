b2a1e9e4805713637f0a1587fcc37923d9a95db1
Shahadat Hossain <shahadat3669@gmail.com>
2022-11-28 23:23:27 +0600

    Display books from local storage.

--- js/main.js
+++ js/main.js
@@ -5,19 +5,25 @@ const bookTitle = document.querySelector('#bookTitle');
 const authorName = document.querySelector('#authorName');
 const isbnNumber = document.querySelector('#isbnNumber');
 const allBooks = document.querySelector('.all-books');
-
+const removeBookBtn = document.querySelector('.removeBookBtn');
 function displayBook() {
-  const allBook = bookCollection.map(
-    (item) => `<div class="book">
+  let htmlGenerate = '';
+  bookCollection.map(
+    (item) =>
+      (htmlGenerate += `<div class="book">
     <h3>${item.bookTitle}</h3>
     <p>
      ${item.authorName}
     </p>
-    <button class="btn">Remove</button>
-  </div>`
+    <button class="removeBookBtn">Remove</button>
+  </div>`)
   );
-  allBooks.innerHTML = allBook;
+  allBooks.innerHTML = htmlGenerate;
 }
+
+displayBook();
+
+const removeBook = () => {};
 
 const addBook = (e) => {
   e.preventDefault();
