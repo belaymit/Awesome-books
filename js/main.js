b4a03d92ab9d73219fee87312403ebf2b3d3f8c4
Swarzstein <zorrillagja@gmail.com>
2022-11-29 11:49:43 -0400

    creating books with Book class

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-const bookCollection = JSON.parse(localSto
-rage.getItem('bookInfo') || '[]');
+const bookCollection = JSON.parse(localStorage.getItem('bookInfo') || '[]');
 const submitBtn = document.querySelector('#submitBtn');
 const bookTitle = document.querySelector('#bookTitle');
 const authorName = document.querySelector('#authorName');
 const isbnNumber = document.querySelector('#isbnNumber');
+let n = bookCollection[bookCollection.length-1].id;
+class Book {
+  constructor (id, title, author){
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
 
+  addBook = (e) => {
+    e.preventDefault();
+    n += 1;
+    const addNewBookData = new Book(n, bookTitle.value, authorName.value);
+    console.log(addNewBookData);
+    if (bookTitle.value && authorName.value && isbnNumber.value) {
+      bookCollection.push(addNewBookData);
+      localStorage.setItem('bookInfo', JSON.stringify(bookCollection));
+      // displayBook();
+      bookTitle.value = '';
+      authorName.value = '';
+      isbnNumber.value = '';
+    }
+  };
+}
 const addBook = (e) => {
   e.preventDefault();
-  const addNewBookData = {
-    bookTitle: bookTitle.value,
-    authorName: authorName.value,
-    isbnNumber: isbnNumber.value,
-  };
-
+  n += 1;
+  const addNewBookData = new Book(n, bookTitle.value, authorName.value);
+  console.log(addNewBookData);
   if (bookTitle.value && authorName.value && isbnNumber.value) {
     bookCollection.push(addNewBookData);
     localStorage.setItem('bookInfo', JSON.stringify(bookCollection));
