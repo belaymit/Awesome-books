d34e6b5db25be01b6290dc20ace2c4d6031ff5df
Swarzstein <zorrillagja@gmail.com>
2022-11-30 14:14:02 -0400

    added style to the active nav

--- js/main.js
+++ js/main.js
@@ -81,26 +81,35 @@ const addBook = (e) => {
 bookCollection.displayBookCollection();
 submitBtn.addEventListener('click', addBook);
 
-const bookList = document.getElementById('nav-booklist');
-const addNewBook = document.getElementById('nav-add');
-const contact = document.getElementById('nav-contact');
+const bookList = document.querySelector('#nav-booklist a');
+const addNewBook = document.querySelector('#nav-add a');
+const contact = document.querySelector('#nav-contact a');
 const bookListSection = document.getElementById('bookList');
 const addNewBookSection = document.getElementById('addNewBook');
 const contactSection = document.getElementById('contact');
 
 bookList.addEventListener('click', () => {
+  bookList.classList.add('active');
+  addNewBook.classList.remove('active');
+  contact.classList.remove('active');
   bookListSection.classList.remove('hidden');
   addNewBookSection.classList.add('hidden');
   contactSection.classList.add('hidden');
 });
 
 addNewBook.addEventListener('click', () => {
+  bookList.classList.remove('active');
+  addNewBook.classList.add('active');
+  contact.classList.remove('active');
   bookListSection.classList.add('hidden');
   addNewBookSection.classList.remove('hidden');
   contactSection.classList.add('hidden');
 });
 
 contact.addEventListener('click', () => {
+  bookList.classList.remove('active');
+  addNewBook.classList.remove('active');
+  contact.classList.add('active');
   bookListSection.classList.add('hidden');
   addNewBookSection.classList.add('hidden');
   contactSection.classList.remove('hidden');
